b495f7cd2f57b2fa69d273382c7ed6ef34010892
Amospelumi <oluwapelumiabiodun45@gmail.com>
2022-01-26 23:07:22 +0100

    added my map

--- map/views.py
+++ map/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+import folium
 
 
 # Create your views here.
 def map(request):
-    return render(request, "index.html")
+    # creating map object
+    m = folium.Map(location=[19, -12], zoom_start=2)
+    folium.Marker([9.0820, 8.6753]).add_to(m)
+
+    # get html representation of the map object above
+    m = m._repr_html_()
+
+    context = {
+        'm':m,
+    }
+    return render(request, "index.html", context)
 
