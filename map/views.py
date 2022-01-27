7c4d6438b817858fe94d66172d1a6d881867df47
Amospelumi <oluwapelumiabiodun45@gmail.com>
2022-01-27 12:34:44 +0100

    update

--- map/views.py
+++ map/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from .models import Search
+
+# i install and import folium a python module to get the map i need
 import folium
+
 import geocoder
 
 # Create your views here.
 def map(request):
+    # Country = request.POST.get("address")
+    Country = Search.objects.all().last()
+    location = geocoder.osm(Country)
+    lat = location.lat
+    lng = location.lng
+    country = location.country
+    
     # creating map object
     m = folium.Map(location=[19, -12], zoom_start=2)
-    folium.Marker([9.0820, 8.6753], tooltip='Click for more', popup="Nigeria").add_to(m)
+    folium.Marker([lat, lng], tooltip='Click for more', popup=country, icon=folium.Icon(color='red',icon_color='white')).add_to(m)
 
     # get html representation of the map object above
     m = m._repr_html_()
