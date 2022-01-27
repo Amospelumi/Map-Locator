ec468825039f04071c49c35ce5c6001dcb95016b
Amospelumi <oluwapelumiabiodun45@gmail.com>
2022-01-27 10:19:44 +0100

    updated

--- map/views.py
+++ map/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 import folium
-
+import geocoder
 
 # Create your views here.
 def map(request):
     # creating map object
     m = folium.Map(location=[19, -12], zoom_start=2)
-    folium.Marker([9.0820, 8.6753]).add_to(m)
+    folium.Marker([9.0820, 8.6753], tooltip='Click for more', popup="Nigeria").add_to(m)
 
     # get html representation of the map object above
     m = m._repr_html_()
